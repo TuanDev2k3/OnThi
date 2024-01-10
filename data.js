9da26488036cebdcdc7febb6e39623ca79bf49ec
TuanDe2k3 <tuancoder2k3@gmail.com>
2024-01-10 22:29:03 +0700

    oke

---
+++ data.js
@@ -0,0 +1,405 @@
+let Chuong1 =[
+    {
+        id:  1,
+        quest: " Quá trình sản xuất là gì?",
+        answers: ["  Là quá trình con người tác động vào tài nguyên thiên nhiên để biến nó thành sản phẩm",
+                " Là quá trình con người tác động vào tài nguyên thiên nhiên để biến nó thành sản phẩm phục vụ cho lợi ích của con người. ",
+                " Là quá trình biến nguyên vật liệu hành sản phẩm phục vụ cho lợi ích của con người. ",
+                " Là quá trình tạo thành sản phẩm phục vụ cho lợi ích của con người. "],
+        correct: 1
+    },
+
+{
+        id:  2,
+        quest: " Quá trình sản xuất trong nhà máy cơ khí bao gồm:",
+        answers: [" Quá trình thiết kế sản phẩm ",
+                "  Quá trình gia công",
+                " Quá trình kiểm tra, đóng gói  ",
+                "  Tập hợp các hoạt động có ích để biến nguyên vật liệu hay bán thành phẩm thành sản phẩm."],
+        correct: 3
+    },
+
+{
+        id:  3,
+        quest: "  Quá trình công nghệ làm thay đổi những tính chất gì của chi tiết ",
+        answers: [" Hình dáng, kích thước, tính chất lý hóa, vị trí tương quan giữa các chi tiết trong sản phẩm ",
+                "  Tính chất lý hóa",
+                "  Vị trí tương quan giữa các chi tiết trong sản phẩm",
+                " Hình dáng, kích thước  "],
+        correct: 0
+    },
+
+{
+        id: 4 ,
+        quest: "  Quá trình sản xuất chính là quá trình liên quan trực tiếp đến chế tạo chi tiết, lắp ráp và hoàn chỉnh sản phẩm bao gồm:",
+        answers: [" Quá trình tạo phô ",
+                " Quá trình gia công cắt gọt ",
+                "  Quá trình lắp ráp, đóng gói",
+                "  Tất cả quá trình trên"],
+        correct: 3
+    },
+
+{
+        id:  5,
+        quest: "  Quá trình công nghệ gia công cơ là quá trình:",
+        answers: [" Cắt gọt phôi và làm thay đổi kích thước hình dạng ",
+                "  Làm thay đổi tính chất vật lý và hóa học của vật liệu chi tiết",
+                " Tạo mối quan hệ tương quan giữa các chi tiết",
+                " Tất cả quá trình trên đều sa "],
+        correct: 0
+    },
+
+{
+        id: 6 ,
+        quest: "  Quá trình công nghệ gia công nhiệt luyện là quá trình",
+        answers: [" Cắt gọt phôi và làm thay đổi kích thước hình dạng ",
+                " Làm thay đổi tính chất vật lý và hóa học của vật liệu chi tiết ",
+                "  Tạo mối quan hệ tương quan giữa các chi tiết",
+                " Tất cả quá trình trên đều sai "],
+        correct: 1
+    },
+
+{
+        id:  7,
+        quest: "  Quá trình công nghệ gia công lắp ráp là quá trình: ",
+        answers: ["  Cắt gọt phôi và làm thay đổi kích thước hình dạng",
+                "  Làm thay đổi tính chất vật lý và hóa học của vật liệu chi tiết ",
+                " Tạo mối quan hệ tương quan giữa các chi tiết",
+                "  Tất cả quá trình trên đều sai"],
+        correct: 2
+    },
+
+{
+        id:  8,
+        quest: " Quá trình công nghệ gia công chế tạo phôi là quá trình:",
+        answers: ["  Cắt gọt phôi và làm thay đổi kích thước hình dạng",
+                "  Làm thay đổi tính chất vật lý và hóa học của vật liệu chi tiết Tạo mối quan hệ tương quan giữa các chi tiết",
+                "  Tất cả quá trình trên đều sai"],
+        correct: 3
+    },
+
+{
+        id:  9,
+        quest: "  Thay đổi yếu tố nào sau đây thì ta chuyển sang nguyên công khác:",
+        answers: [" Thay đổi vị trí làm việc ",
+                " Thay đổi chế độ cắt ",
+                "  Thay đổi dụng cụ cắt ",
+                " Cả 3 câu trên đều đúng "],
+        correct: 0
+    },
+
+{
+        id:  10,
+        quest: "  Trong một nguyên công có thể có bao nhiêu lần gá",
+        answers: ["  Một lần gá ",
+                " Hai lần gá  ",
+                " Ba lần gá ",
+                "  Có ít nhất một lần gá"],
+        correct: 3
+    },
+
+{
+        id:  11,
+        quest: "  Trong một lần gá có bao nhiêu vị trí ",
+        answers: ["  Một vị trí",
+                " Hai vị trí ",
+                " Ba vị trí",
+                " Có ít nhất một vị trí"],
+        correct: 3
+    },
+
+{
+        id:  12,
+        quest: "  Thay đổi yếu tố nào sau đây thì ta chuyển sang một bước mới ",
+        answers: ["  Thay đổi bề mặt gia công",
+                " Thay đổi dụng cụ cắt  ",                
+" Thay đổi chế độ cắt  ",
+                "  Cả 3 câu trên đều đúng"],
+        correct: 3
+    },
+
+{
+        id:  13,
+        quest: "  Khi sản xuất mà số lượng sản phẩm hàng năm ít, sản phẩm không ổn định thì người ta gọi là dạng sản xuất",
+        answers: ["  Đơn chiếc",
+                "  Hàng loạt",
+                "  Hàng khối",
+                "  Cả 3 câu trên đều sai"],
+        correct: 0
+    },
+
+{
+        id:  14,
+        quest: "  Khi sản xuất mà số lượng sản phẩm hàng năm lớn, sản phẩm ổn định thì người ta gọi là dạng sản xuất:  ",
+        answers: ["  Đơn chiếc",
+                "  Hàng loạt",
+                "  Hàng khối",
+                "  Cả 3 câu trên đều sai"],
+        correct: 2
+    },
+
+{
+        id:  15,
+        quest: "  Hình thức tổ chức sản xuất theo dây chuyền phù hợp với dạng sản xuất nào?",
+        answers: ["  Đơn chiếc, hàng loạt nhỏ ",
+                "  Đơn chiếc, hàng loạt lớn ",
+                " Hàng khối, hàng loạt lớn  ",
+                "  Hàng khối, hàng loạt nhỏ "],
+        correct: 2
+    },
+
+{
+        id: 16 ,
+        quest: "  Sản phẩm cơ khí là: ",
+        answers: [" Chi tiết kim loại thuần túy ",
+                "  Bộ phận máy gồm các chi tiết kim loại và không kim loại ",
+                "  Một máy hoàn chỉnh",
+                " Cả 3 câu trên đều đúng"],
+        correct: 3
+    },
+
+{
+        id: 17 ,
+        quest: "  Trong một bước có bao nhiêu đường chuyển dao ",
+        answers: [" Có một đường chuyển dao ",
+                "  Có hai đường chuyển dao",
+                " Có nhiều đường chuyển dao",
+                " Có ít nhất một đường chuyển dao "],
+        correct: 3
+    },
+
+{
+        id:  18,
+        quest: "  Để tiện một đoạn trụ bậc, người ta chia ra làm các lát cắt: 3 lát cắt thôi cùng chiều sâu, 2 lát cắt bán tinh, 1 lát cắt tinh. Vậy quá trình trên gồm máy bước:",
+        answers: ["  1 bước",
+                " 2 bước ",
+                " 3 bước ",
+                "  4 bước"],
+        correct: 2
+    },
+
+{
+        id:  19,
+        quest: "  Để phân loại các dạng sản xuất người ta dựa vào những yếu tố nào:",
+        answers: [" Sản lượng sản phẩm hàng năm và số lượng sản phẩm từng lần đặt hàng ",
+                "  Mức độ ổn định của sản lượng và số lượng sản phẩm từng lần lô hàng",
+                "  Số lượng sản phẩm trong lô hàng",
+                " Mức độ ổn định và sản lượng hàng năm"],
+        correct: 3
+    },
+
+{
+        id:  20,
+        quest: " Trong trường hợp gia công chi tiết phức tạp, chúng ta có máy tổ hợp thì nên sử dụng phương án: ",
+        answers: ["  Tập trung nguyên công",
+                "  Phân tán nguyên công",
+                "  Không tập trung và không phân tán nguyên công",
+                "  Tập trung và phân tán nguyên công"],
+        correct: 0
+    },
+
+{
+        id: 21 ,
+        quest: "  Đơn vị nhỏ nhất của nguyên công là:",
+        answers: ["  Vị trí",
+                "  Đường chuyển dao",
+                "  Động tác",
+                " Bước"],
+        correct: 2
+    },
+
+{
+        id: 22 ,
+        quest: "  Quá trình liên quan trực tiếp đến việc làm thay đổi hình dáng, kích thước, tính chất và tạo ra mối quan hệ giữa các chi tiết là quá trình: ",
+        answers: ["  Quá trình công nghệ",
+                "  Quá trình sản xuất",
+                "  Quá trình gia công",
+                "  Quá trình lắp ráp "],
+        correct: 1
+    },
+
+{
+        id: 23 ,
+        quest: "  Chúng ta sử dụng phương án phân tán nguyên công khi: ",
+        answers: [" Chi tiết gia công phức tạp, có các máy móc chuyên dùng",
+                "  Khi gia công chi tiết đơn giản, có máy móc chuyên dùng",
+                " Khi gia công chi tiết đơn giản, có máy móc tổ hợp",
+                " Khi gia công chi tiết phức tạp, có máy móc tổ hợp "],
+        correct: 2
+  },
+
+{
+        id:  24,
+        quest: "  Bộ phận nghiên cứu và phát triển sản phẩm làm nhiệm vụ",
+        answers: ["Nghiên cứu cải tiến sản phẩm cũ  ",
+                " Nghiên cứu cải tiến sản phẩm mới ",
+                " Nghiên cứu công nghệ mới và đưa vào ứng dụng ",
+                "  Các câu còn lại đều đúng"],
+        correct: 3
+    },
+{
+        id:  25,
+        quest: "  Sản xuất theo dây chuyền đem lại tính hiệu quả kinh tế cao là vì:  ",
+        answers: ["  Giảm thời gian phụ",
+                " Không phụ thuộc tay nghề công nhân",
+                "  Dễ đạt độ chính xác",
+                "  Cả 3 câu trên đều đúng"],
+        correct: 3
+    },
+{
+        id:  26,
+        quest: "  Hành động điều khiển máy của công nhân được gọi là:",
+        answers: ["  Động tác",
+                "  Nguyên công",
+                " Quá trình công nghệ ",
+                "  Bước"],
+        correct: 0
+    },
+{
+        id: 27 ,
+        quest: " Đường chuyển dao là một phần của bước dùng để hớt đi một phần vật liệu bằng một dụng cụ cắt và …. ",
+        answers: [" Cùng một máy gia công ",
+                "  Cùng một chiều sâu cắt",
+                " Cùng một chế độ cắt",
+                "  Cùng một bước tiến dao"],
+        correct: 2
+    },
+{
+        id: 28 ,
+        quest: "  Bước là một phần của nguyên công dùng để tiến hành gia công một bề mặt sử dụng 1 dụng cụ cắt và … ",
+        answers: [" Cùng một máy gia công ",
+                "  Cùng một chiều sâu cắt",
+                " Cùng một chế độ cắt   ",
+                " Cùng một bước tiến dao "],
+        correct: 2
+    },
+{
+        id: 29 ,
+        quest: "  ….. Là một phần của nguyên công được hoàn thành trong một lần gá ",
+        answers: ["  Bước",
+                " Gá  ",
+                " Vị trí ",
+                "  Động tác"],
+        correct: 2
+    },
+{
+        id:  30,
+        quest: "  Tìm đáp án sai trong câu: “ Vị trí được xác định bởi một vị trí tương quan giữa chi tiết máy với ….. “  ",
+        answers: ["  Máy",
+                " Công nhân ",
+                "  Chuẩn so",
+                "  Dụng cụ cắt"],
+        correct: 2
+    },
+
+{
+        id:  31,
+        quest: "  Quá trình công nghệ được hoàn thiện rồi ghi lại thành văn kiện công nghệ được gọi là gì?",
+        answers: ["  Quy trình sản xuất",
+                " Quá trình sản xuất ",
+                "  Quy trình công nghệ",
+                " Quy trình nguyên công"],
+        correct: 3
+    },
+
+{
+        id: 32 ,
+        quest: "  Theo TCVN hệ số chuyên môn hóa Kc = 250 thì dạng sản xuất của sản phẩm là:",
+        answers: [" Đơn chiếc ",
+                " Loạt lớn ",
+                "  Hàng khối",
+                " Loạt nhỏ "],
+        correct: 0
+    },
+
+{
+        id:  33,
+        quest: "  Theo TCVN hệ số chuyên môn hóa Kc = 2 thì dạng sản xuất của sản phẩm là:",
+        answers: [" Đơn chiếc ",
+                " Loạt lớn ",
+                "  Hàng khối",
+                " Loạt nhỏ "],
+        correct: 3
+    },
+
+{
+        id: 34 ,
+        quest: "  Theo TCVN hệ số chuyên môn hóa Kc = 5 thì dạng sản xuất của sản phẩm là:",
+        answers: [" Đơn chiếc ",
+                " Loạt lớn ",
+                "  Hàng khối",
+                " Loạt nhỏ "],
+        correct: 1
+    },
+
+{
+        id: 35 ,
+        quest: " Theo TCVN hệ số chuyên môn hóa Kc = 112 thì dạng sản xuất của sản phẩm là:",
+        answers: [" Đơn chiếc ",
+                " Loạt lớn ",
+                "  Hàng khối",
+                " Loạt nhỏ "],
+        correct: 3
+    },
+
+{
+        id:  36,
+        quest: "  Trực tiếp làm thay đổi hình dáng, kích thước, vị trí tương quan và tính chất cơ lý của chi tiết máy là: ",
+        answers: ["  Nguyên công",
+                "  Bước",
+                "  Quá trình công nghệ",
+                "  Các câu còn lại đều đúng"],
+        correct: 2
+    },
+
+{
+        id:  37,
+        quest: "  ….. là một phần của quá trình công nghệ được hoàn thành liên tục, tại một chỗ làm việc và do một hoặc một nhóm công nhân thực hiện ",
+        answers: ["  Nguyên công",
+                "  Bước",
+                " Quá trình công nghệ ",
+                " Các câu còn lại đều đúng "],
+        correct: 0
+    },
+
+{
+        id: 38 ,
+        quest: " Các thành phần nào thuộc quá trình công nghệ",
+        answers: ["Nguyên công, gá ",
+                "  Bước, đường chuyển dao",
+                " Động tác, vị trí ",
+                "  Các câu còn lại đều đúng"],
+        correct: 3
+    },
+
+{
+        id: 39 ,
+        quest: "  Máy có thể thực hiện nhiều nguyên công khác nhau của nhiều loại chi tiết khác nhau gọi là ",
+        answers: ["  Máy vạn năng",
+                "  Máy chuyên dùng",
+                " Máy chuyên môn hóa",
+                " Máy tự động"],
+        correct: 0
+    },
+
+{
+        id: 40 ,
+        quest: " Máy dùng để gia công một loại hay một vài loại chi tiết có hình dạng tương tự, nhưng kích thước khác nhau ",
+        answers: [" Máy vạn năng ",
+                "  Máy chuyên dùng",
+                "  Máy chuyên môn hóa",
+                "  Máy tự động"],
+        correct: 1
+    },
+
+{
+        id:  41,
+        quest: "  Bộ phận nghiên cứu và phát triển sản phẩm không làm nhiệm vụ.",
+        answers: [" Nghiên cứu nguyên liệu tái chế. ",
+                "  Nghiên cứu phát triển sản phẩm mới.",
+                "  Nghiên cứu công nghệ mới và đưa vào ứng dụng.",
+                "  Phân tích thị hiếu khách hàng."],
+        correct: 0
+    },
+
+]
